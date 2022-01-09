869a961c181e8d53b57e0fe437dd0f94ff1a9c86
Vitalina5 <viktoriana15@gmail.com>
2022-01-09 21:16:27 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import image from './shopping.jpg';
+import imageTwo from './man.jpg';
+import { CroceryList } from './GroceryList';
+
+
+function App() {
+  return (
+    <div className='app'>
+    <div className='container'>
+    <img src={image} width="200px" alt="shopping"/>npm
+    </div>
+    <div className='container'>
+    <h1>Grocery List</h1>
+    </div>
+    <CroceryList />
+    <div className='container'>
+    <img src={imageTwo} width="200px" alt="man" />
+    </div>
+    </div>
+  );
+}
+
+export default App;
+
